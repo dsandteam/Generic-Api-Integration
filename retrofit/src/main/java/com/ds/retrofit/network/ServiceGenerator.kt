e6cb28fedd6adb728cc8f131feda5799e8e40ac6
Amir Khan <mohd.amir@dotsquares.com>
2022-09-27 12:52:35 +0530

    * added request logger for Android 12

--- retrofit/src/main/java/com/ds/retrofit/network/ServiceGenerator.kt
+++ retrofit/src/main/java/com/ds/retrofit/network/ServiceGenerator.kt
@@ -1,6 +1,7 @@
 package com.ds.retrofit.network
 
 import android.content.Context
+import com.amir.chuck.RequestLogger
 import com.ds.retrofit.BuildConfig
 import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
@@ -49,6 +50,9 @@ object ServiceGenerator {
             writeTimeout(timeOut, TimeUnit.SECONDS)
             addInterceptor(requestInterceptor)
             addInterceptor(loggingInterceptor)
+            if (BuildConfig.DEBUG){
+                addInterceptor(RequestLogger(context))
+            }
             connectivityInterceptor?.let {
                 addInterceptor(connectivityInterceptor)
             } ?: addInterceptor(ConnectivityInterceptorImpl(context,null))
